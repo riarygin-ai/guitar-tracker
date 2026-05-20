37a50349b64f103750f39fc4778f9e6a76255b75
Roman Iarygin <r.iarygin@gmail.com>
2026-05-20 17:07:57 -0400

    Remove local env file from git

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -14,7 +14,7 @@ import type {
 } from '@/types';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
 
 if (!supabaseUrl) {
