2c651614b9c4d848b28e927aab7e567e178754bb
Roman Iarygin <r.iarygin@gmail.com>
2026-05-20 17:18:13 -0400

    Remove local env file from git

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -22,7 +22,7 @@ if (!supabaseUrl) {
 }
 
 if (!supabaseAnonKey) {
-  throw new Error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY')
+  throw new Error('Missing NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY')
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
